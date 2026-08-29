62b3b0de39cae65ad84caa1804688cc155993453
Corentin MAS <corentin@ctn-solutions.com>
2026-08-30 00:10:53 +0200

    ci: dump present metric names on failure

--- test/e2e/e2e.sh
+++ test/e2e/e2e.sh
@@ -202,7 +202,8 @@ metrics_exposed() {
     keycloak_operator_admin_requests_total \
     keycloak_operator_admin_request_duration_seconds_bucket; do
     echo "$M" | grep -q "^$metric" || {
-      echo "missing: $metric (fetched $(echo "$M" | wc -l) lines, $(echo "$M" | grep -c keycloak_operator) keycloak lines, image: $(kubectl -n keycloak-operator-system get deployment keycloak-operator -o jsonpath='{.spec.template.spec.containers[0].image}'))" >&2
+      echo "missing: $metric (fetched $(echo "$M" | wc -l) lines, image: $(kubectl -n keycloak-operator-system get deployment keycloak-operator -o jsonpath='{.spec.template.spec.containers[0].image}'))" >&2
+      echo "$M" | grep -oE '^keycloak_operator[a-z_]*' | sort -u | sed 's/^/  present: /' >&2
       return 1
     }
   done
